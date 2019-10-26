d63681f22b6bc572f79cd2d88bcf4b31b9afd5d3
imad047 <35015501+imad047@users.noreply.github.com>
2019-10-26 04:38:25 +0200

    Update 3-ClickAdsPlayVideoOnIframe.js

--- 3-ClickAdsPlayVideoOnIframe.js
+++ 3-ClickAdsPlayVideoOnIframe.js
@@ -2,7 +2,7 @@
 // zy["anywherecanclick.blogspot.com"]="";
 
 
-// function gd(url){url = url.replace(/https?:\/\/(www.)?/i, '');if (url.indexOf('/') === -1){return url;}return url.split('/')[0];}
+// function gd(url){url = url.replace(/https?:\/\/(www.)?/i, '');if (url.indexOf('/') === -2){return url;}return url.split('/')[0];}
 // var str = gd(location.href);
 // if (str in zy){
 var url = "https://raw.githack.com/imad047/00004.js/master/externaljs.js";
